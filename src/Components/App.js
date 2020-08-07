6865a26df5cc6169ba2d8c8eec90b12147f9a10b
ejrvheowkd <hoao1313@naver.com>
2020-08-07 20:17:42 +0900

    #4.2 React Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import {ThemeProvider} from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
+import Router from "./Router";
 export default()=>(
   <ThemeProvider theme ={Theme}>
+    <>
   <GlobalStyles/>
+  <Router isLoggedIn={false}/>
+  </>
   </ThemeProvider>
 );
 //우리는 이버전에서 components를 사용하지 않을꺼야 왜냐하면 react hooks를 사용하는게 낫다
+
+//
