81ef6bd6d7c4d2ec5fc184be4474f97eb8eb0721
ejrvheowkd <hoao1313@naver.com>
2020-08-14 01:01:59 +0900

    #5.0 Auth Route UI part One

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,6 +1,6 @@
 // styled component에서 Theme을 갖고 작업을 하는 좋다 비쿼즈 나 대신 색상을 복붙해주거든
 const BOX_BORDER = "1px solid #dbdbdb";
-const BODER_RADIUS = "4px";
+const BORDER_RADIUS = "4px";
 export default{
     bgColor:"#FAFAFA",
     blackColor:"#262626",
@@ -10,9 +10,9 @@ export default{
     blueColor:"#0095f6",
     darkBlueColor:"#00376b",
     boxBorder:"1px solid #dbdbdb",
-    boarderRadius:"4px",
-    whiteBox: `${BODER_RADIUS};
-               ${BOX_BORDER};
-               background0color:white;
-               `
+    borderRadius:"4px",
+    whiteBox: `border:${BOX_BORDER};
+             border-radius:${BORDER_RADIUS};
+             background-color:white;
+            `
 };
