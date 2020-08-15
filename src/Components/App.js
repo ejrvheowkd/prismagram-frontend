94c2ded838ad14a1f1a26a112d7a00416aeb46a7
ejrvheowkd <hoao1313@naver.com>
2020-08-16 00:26:56 +0900

    #5.1 Footer and Auth UI part Two

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,36 @@
 import React from "react";
-import {gql} from "apollo-boost";//gql===graphql
-import {ThemeProvider} from "styled-components";
+import {gql} from "apollo-boost"; //gql===graphql
+import styled, {ThemeProvider} from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
 import {useQuery} from "react-apollo-hooks";
+import Footer from "./Footer";
 
-const QUERY = gql`
+const QUERY = gql `
 {
   isLoggedIn @client
 }
 `;
+const Wrapper = styled.div `
+margin: 0 auto;
+max-width: 935px;
+width: 100%;
+`;
 //@client가 react apollo 없으면 쿼리를 API로 보내려고 한다
-export default()=>{
+export default() => {
 
-  const {data:{isLoggedIn}} = useQuery(QUERY);
-  return(
-  <ThemeProvider theme ={Theme}>
-    <>
-  <GlobalStyles/>
-  <Router isLoggedIn={isLoggedIn}/>
-  </>
-  </ThemeProvider>
-  );
+    const {data: {
+            isLoggedIn
+        }} = useQuery(QUERY);
+    return (
+        <ThemeProvider theme={Theme}>
+            <Wrapper>
+                <GlobalStyles/>
+                <Router isLoggedIn={isLoggedIn}/>
+                <Footer/>
+            </Wrapper>
+        </ThemeProvider>
+    );
 };
 //이게 react-apollo-hooks를 이용해서 query를 수행하는 데 필ㄹ요한 코드
--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,24 +1,44 @@
-import React,{useState}from "react";
-import styled from "styled-components";
+import React, {useState} from "react";
+import styled from "styled-components"; //라이브러리
 
-const Wrapper = styled.div`
+const Wrapper = styled.div `
     min-height: 80vh;
     display:flex;
     align-items:center;
     justify-content:center;
     `;
 
-const Box = styled.div`
-${props=>props.theme.whiteBox}
+const Box = styled.div `
+${props => props.theme.whiteBox}
+border-radius:0px;
 `;
-export default () =>{
-    const [action,setAction] = useState("logIn");
+
+const StateChanger = styled(Box)`
+    text-align:center;
+    padding:20px 0px;
+`
+
+const Link = styled.span`
+  color: ${props => props.theme.blueColor};
+  cursor: pointer;
+`;
+export default() => {
+    const [action, setAction] = useState("logIn");
 
     return (
         <Wrapper>
-            {action==="logIn"?<Box>
-                Log iN
-                </Box>:<Box>Sign Up</Box>}
+            <StateChanger>
+                {
+                    action === "logIn"
+                        ? (
+                            <> 
+                            Don 't have an account?{" "}
+                            < Link onClick = { () => setAction("signUp")} > Sign up</Link> </>
+        ) : (
+          <>
+            Have an account?{" "}
+            <Link onClick={() => setAction("logIn")}>Log in</Link > </> )}
+                </StateChanger>
         </Wrapper>
     );
 };
