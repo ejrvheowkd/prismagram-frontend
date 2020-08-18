dae9af071f9b6b926e1aa14e6a7904d4edb6da9b
ejrvheowkd <hoao1313@naver.com>
2020-08-18 17:43:23 +0900

    #5.5 Toastify and createAccount Mutation

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -4,6 +4,7 @@ import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { LOG_IN } from "./AuthQueries";
 import { useMutation } from "react-apollo-hooks";
+import { toast } from "react-toastify";
 
 //useInput은 나에게 value랑 onChange를 줄거야
 export default() => {
@@ -11,13 +12,51 @@ export default() => {
     const username = useInput("");
     const firstName = useInput("");
     const lastName = useInput("");
-    const email = useInput("");
-    const [requestSecret] = useMutation(LOG_IN,{variables:{email:email.value}});//mutation은 쿼리가 있어야해
+    const email = useInput("www@w");
+    const [requestSecret] = useMutation(LOG_IN,
+        {
+        update:(_,{data})=>{//update가 발생하면 result를 얻는다 update는 mutation이 발생하면 실행되는 함수얌
+            const {requestSecret} =data;
+            if(!requestSecret){
+                toast.error("계정이 없습니다.");
+                setTimeout(()=> setAction("signUp"),3000);
+            }
+    }, 
+    variables:{email:email.value}
+    }
+        );//mutation은 쿼리가 있어야해
     //만약 내가 text value로 받고 싶으면 email.value어야 =한다.
-    const onLogin    =(e) =>{
+    const creatAccount = useMutation(CREATE_ACCOUNT,{
+        variables:{
+            email: email.value,
+            usertname: username.value,
+            firstName: firstName.value,
+            lastName: lastName.value
+        }
+    });
+    
+    const onSubmit    =(e) =>{
         e.preventDefault();
-        if(email!==""){
-            requestSecret();
+        if(action==="logIn"){
+            if(email!==""){
+                requestSecret();
+            }else
+            {
+                toast.error("이메일이 필요해용~");
+            }
+        }
+        else if(action==="signUp"){
+            if(email.value!==""&&
+                username.value!==""&&
+                firstName.value!==""&&
+                lastName.value!=="")
+                {
+                    createAccount();
+                }
+                else
+                {
+                    toast.error("모든 칸 채웠!");
+                }
         }
     };
     return (
@@ -28,7 +67,7 @@ export default() => {
         firstName={firstName}
         lastName ={lastName}
         email={email}
-        onLogin={onLogin}
+        onSubmit={onLogin}
         />
     )
 };
