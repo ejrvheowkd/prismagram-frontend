e53ca41f048c62b6588754fb9dc86fd58e6bf970
ejrvheowkd <hoao1313@naver.com>
2020-08-25 18:02:32 +0900

    #6.1 Header Logic part One

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -3,18 +3,24 @@ import {Route,Switch} from "react-router-dom";
 import React from "react";
 import Feed from "../Routes/Feed";
 import Auth from "../Routes/Auth";
+import Explore from "../Routes/Explore";
+import Profile from "../Routes/Profile";
+import Search from "../Routes/Search";
 const LoggedInRoutes = () => (
-<>
+<Switch>
 <Route exact path="/" component={Feed}/>
-</>
+<Route  path="/explore" component={Explore}/>
+<Route  path="/search" component ={Search}/>
+<Route path="/:username" component={Profile} />
+</Switch>//Switch는 딱 하나의 라우트만 렌더링해준다
 );
 const LoggedOutRoutes = () => (
-<>
+<Switch>
 <Route exact path="/" component ={Auth} />
-</>
+</Switch>
 );
 const AppRouter = ({isLoggedIn}) => (
-     <Switch>{isLoggedIn?<LoggedInRoutes/>:<LoggedOutRoutes/>}</Switch>
+  isLoggedIn?<LoggedInRoutes/>:<LoggedOutRoutes/>
 );
 
 
